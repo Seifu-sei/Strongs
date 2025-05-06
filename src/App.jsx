1278da14d577355d4fbba1d431398ff6c8c5f493
Godswork <95004389+Godswork4@users.noreply.github.com>
2025-05-06 11:08:25 +0100

    Update App.jsx
    ensured all routing is working fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router } from "react-router-dom";
+// App.jsx (no router needed here)
 import AppRoutes from "./routes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <AppRoutes />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <AppRoutes />
+      </main>
+      <Footer />
+    </div>
   );
 }
