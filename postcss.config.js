c8424d6cb6714ccfcbff1af54b4f6d512c4ddfbf
Godswork <95004389+Godswork4@users.noreply.github.com>
2025-08-08 18:08:34 +0100

    Fix merge conflicts in configuration files

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 export default {
-=======
-module.exports = {
->>>>>>> 1b5c486f02108dde67b53c5e5f4df1547fd6128d
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> 1b5c486f02108dde67b53c5e5f4df1547fd6128d
